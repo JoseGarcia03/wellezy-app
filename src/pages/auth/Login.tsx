5d9cec52ffae43a0426a9c0755a597ce58a3cdcc
Jose Garcia <josegarciajaime02@gmail.com>
2024-11-03 11:41:59 -0500

    added default email and password

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from "react";
+import React, { useContext, useState } from "react";
 import {
     Alert,
     Box,
@@ -6,33 +6,39 @@ import {
     FormLabel,
     IconButton,
     InputAdornment,
-    OutlinedInput, Snackbar, TextField,
-    Typography
+    OutlinedInput,
+    Snackbar,
+    TextField,
+    Typography,
 } from "@mui/material";
-import LoadingButton from '@mui/lab/LoadingButton';
-import {IoMdEye, IoMdEyeOff} from "react-icons/io";
-import {DotLottieReact} from "@lottiefiles/dotlottie-react";
+import LoadingButton from "@mui/lab/LoadingButton";
+import { IoMdEye, IoMdEyeOff } from "react-icons/io";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 import useForm from "../../hooks/useForm.ts";
 import axios from "axios";
-import {useMutation} from "@tanstack/react-query";
-import {Link} from "react-router-dom";
-import {AuthContext} from "../../context/AuthContext.tsx";
+import { useMutation } from "@tanstack/react-query";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext.tsx";
 import LoadingBackdrop from "../../components/LoadingBackdrop.tsx";
 
 type FormData = {
     email: string;
     password: string;
-}
+};
 const Login: React.FC = () => {
     const authContext = useContext(AuthContext);
 
     const [showNotify, setShowNotify] = useState<boolean>(false);
     const [errorMessage, setErrorMessage] = useState<string>("");
-    const [showPassword,setShowPassword] = useState(false);
-    const { values, handleChange } = useForm<FormData>({ email: "", password: ""});
+    const [showPassword, setShowPassword] = useState(false);
+    const { values, handleChange } = useForm<FormData>({
+        email: "admin@wellezy.com",
+        password: "change_me",
+    });
 
     const mutation = useMutation({
-        mutationFn: async () => await axios.post(`${import.meta.env.VITE_API_URL}/login`, values),
+        mutationFn: async () =>
+            await axios.post(`${import.meta.env.VITE_API_URL}/login`, values),
         onSuccess: ({ data }) => {
             authContext?.setUserData(data.data.user);
             authContext?.login(data.data.token);
@@ -41,24 +47,41 @@ const Login: React.FC = () => {
             // @ts-ignore
             setErrorMessage(error?.response?.data?.message);
             setShowNotify(true);
-        }
+        },
     });
 
     const handleClickShowPassword = () => setShowPassword(!showPassword);
-    const handleSubmit = async(event: React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         mutation.mutate();
-    }
+    };
 
     if (!authContext) return <LoadingBackdrop />;
 
     return (
         <div className={"flex items-center"}>
             <div className={"w-3/5"}>
-                <Typography variant={"h1"} sx={{ fontSize: 24, fontWeight: 500, textAlign: "center", marginBottom: 3 }}>Bienvenido a Wellezy!</Typography>
-                <Box component={"form"} onSubmit={handleSubmit} autoComplete={"off"} className={"mx-auto max-w-sm"}>
+                <Typography
+                    variant={"h1"}
+                    sx={{
+                        fontSize: 24,
+                        fontWeight: 500,
+                        textAlign: "center",
+                        marginBottom: 3,
+                    }}
+                >
+                    Bienvenido a Wellezy!
+                </Typography>
+                <Box
+                    component={"form"}
+                    onSubmit={handleSubmit}
+                    autoComplete={"off"}
+                    className={"mx-auto max-w-sm"}
+                >
                     <FormControl fullWidth sx={{ marginBottom: 3 }}>
-                        <FormLabel htmlFor={"email"}>Correo Electronico</FormLabel>
+                        <FormLabel htmlFor={"email"}>
+                            Correo Electronico
+                        </FormLabel>
                         <TextField
                             id={"email"}
                             name={"email"}
@@ -80,7 +103,7 @@ const Login: React.FC = () => {
                         <OutlinedInput
                             id={"password"}
                             name={"password"}
-                            type={ showPassword ? "text" : "password"}
+                            type={showPassword ? "text" : "password"}
                             size={"small"}
                             value={values.password}
                             autoComplete={"current-password"}
@@ -90,11 +113,19 @@ const Login: React.FC = () => {
                             endAdornment={
                                 <InputAdornment position={"end"}>
                                     <IconButton
-                                        aria-label={ showPassword ? "hide the password" : "diplay the password" }
+                                        aria-label={
+                                            showPassword
+                                                ? "hide the password"
+                                                : "diplay the password"
+                                        }
                                         edge={"end"}
                                         onClick={handleClickShowPassword}
                                     >
-                                        { showPassword ? <IoMdEyeOff /> : <IoMdEye /> }
+                                        {showPassword ? (
+                                            <IoMdEyeOff />
+                                        ) : (
+                                            <IoMdEye />
+                                        )}
                                     </IconButton>
                                 </InputAdornment>
                             }
@@ -111,12 +142,24 @@ const Login: React.FC = () => {
                     </LoadingButton>
                     <Typography>
                         ¿Aun no tienes cuenta?
-                        <Link className={"text-blue-600"} to={"/register"}> Registrate</Link>
+                        <Link className={"text-blue-600"} to={"/register"}>
+                            {" "}
+                            Registrate
+                        </Link>
                     </Typography>
                 </Box>
             </div>
-            <div className={"bg-primary h-screen w-2/5 flex justify-center items-center overflow-hidden"}>
-                <DotLottieReact src={"src/assets/airplane_animation.json"} className={"max-w-full scale-150"} loop autoplay />
+            <div
+                className={
+                    "bg-primary h-screen w-2/5 flex justify-center items-center overflow-hidden"
+                }
+            >
+                <DotLottieReact
+                    src={"src/assets/airplane_animation.json"}
+                    className={"max-w-full scale-150"}
+                    loop
+                    autoplay
+                />
             </div>
             <Snackbar
                 open={showNotify}
@@ -124,11 +167,9 @@ const Login: React.FC = () => {
                 anchorOrigin={{ vertical: "top", horizontal: "right" }}
                 onClose={() => setShowNotify(false)}
             >
-                <Alert severity={"error"}>
-                    {errorMessage}
-                </Alert>
+                <Alert severity={"error"}>{errorMessage}</Alert>
             </Snackbar>
         </div>
-    )
-}
+    );
+};
 export default Login;
